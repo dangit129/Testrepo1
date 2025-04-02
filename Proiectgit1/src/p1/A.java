6bf365d3f7f049b4b0e57803dd817b9edd66e816
user1 <user1@RO-ATH013>
2025-04-02 18:44:59 +0300

    Mesaj 3

---
+++ Proiectgit1/src/p1/A.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Primul mesaj");
+		System.out.println("Al doilea mesaj");
+		System.out.println("Al treilea mesaj");
+	}
+
+}
